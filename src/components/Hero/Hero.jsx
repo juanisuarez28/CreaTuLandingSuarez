8c55774ee6bae4899f54d597a648520c4e0b474a
juanisuarez38 <juanisua3@gmail.com>
2024-07-18 19:02:41 -0300

    actualizacion urls imgs

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,19 +1,24 @@
-import React from 'react'
-import './../../styles/fonts.css';
-import { Box, Button, Flex } from '@chakra-ui/react';
-import './Hero.css'
-import { Link } from 'react-router-dom';
-
+import React from "react";
+import "./../../styles/fonts.css";
+import { Box, Button, Flex } from "@chakra-ui/react";
+import "./Hero.css";
+import { Link } from "react-router-dom";
 
 const Hero = () => {
   return (
-    <Box className='container' >
-        <img className="fondo" src="./src/assets/imgs/fondohero.webp" alt="Logo ComeCabron"/>
-        <img className='haven' src="./src/assets/imgs/haventext.png"></img>
-        <img className='somos' src="./src/assets/imgs/somos.png"></img>
-        <Button className='button-shop'><Link to='/productos'>shop now.</Link></Button>
+    <Box className="container">
+      <img
+        className="fondo"
+        src="/assets/imgs/fondohero.webp"
+        alt="fondo"
+      />
+      <img className="haven" src="/assets/imgs/haventext.png" alt="Haven" />
+      <img className="somos" src="/assets/imgs/somos.png" alt="Somos" />
+      <Button className="button-shop">
+        <Link to="/productos">shop now.</Link>
+      </Button>
     </Box>
-  )
-}
+  );
+};
 
-export default Hero 
+export default Hero;
